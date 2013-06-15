3ad54a9ef6f2c3624765e8bf7706e7dd3225dc0f
kimihito <tatsurotamashiro@gmail.com>
2013-06-15 12:47:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passtory::Application.config.secret_token = '1e9a2577fc5c7b23c2d957b9577e243104f7d91942478fe653c05826b3bf4305da55e2b1538b5aba85d4e7f9b4050aef1b217d241acc28ccc0a34794fff0430b'
